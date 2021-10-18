ff0a82cad5d19b98d52f654362d0c724b05ab0ed
J.C. Burnworth <jcburnworth1@gmail.com>
2021-10-18 14:31:09 +0000

    Refactor to add fct_orders.sql

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,16 @@
+WITH payment AS (
+
+    SELECT
+    id AS payment_id
+    ,orderid AS order_id
+    ,paymentmethod AS payment_method
+    ,status
+    -- Convert cents to dollars
+    ,amount / 100 AS amount
+    ,created AS created_at
+    FROM
+    dbt_jburnworth.payment
+
+)
+
+SELECT * FROM payment
